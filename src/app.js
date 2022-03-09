ac31970fd174408b281590d6075eb89632d91207
Elvis Onobhayedo <elvisonob@yahoo.com>
2022-03-09 08:10:29 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,12 +1,36 @@
 import Header from './components/header'
 
 import initialEmails from './data/emails'
+import {useState} from 'react'
 
 import './styles/app.css'
+import React from 'react'
+import { render } from 'react-dom'
+
 
 function App() {
+
+ 
+
+
+ const [emails, setEmails]=useState(initialEmails)
   // Use initialEmails for state
+
+  const toggleRead = (email) => {
+    email.read=!email.read
+  }
+    
+
+
+
+
+
+
   console.log(initialEmails)
+
+  
+
+
 
   return (
     <div className="app">
@@ -15,7 +39,7 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+            //onClick={() => {}}
           >
             <span className="label">Inbox</span>
             <span className="count">?</span>
@@ -34,12 +58,43 @@ function App() {
               id="hide-read"
               type="checkbox"
               checked={false}
-              // onChange={() => {}}
+              //onChange={() => {}}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{emails.map((displayEmail)=> {
+let emailClass='email'
+if (displayEmail.read) {
+  emailClass+='read'
+} else {
+  emailClass +='unread'
+}
+
+return <li className='email'>
+<div className="select">
+<input
+  className="select-checkbox"
+  onClick={()=>toggleRead(displayEmail)}
+  type="checkbox"/>
+  
+  
+</div>
+<div className="star">
+<input
+  className="star-checkbox"
+  type="checkbox"
+  checked={displayEmail.starred}
+/>
+</div>
+<div className="sender">
+{displayEmail.sender}
+</div>
+<div className="title">
+  {displayEmail.title}
+</div>
+</li>})
+}</main>
     </div>
   )
 }
